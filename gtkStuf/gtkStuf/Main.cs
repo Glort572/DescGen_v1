916e625b52cb3850e5659508493ecbb77f616416
glort572 <cricio.rota@gmail.com>
2022-05-16 06:04:37 +0200

    Added Main.cs on gtkStuf folder

---
+++ gtkStuf/gtkStuf/Main.cs
@@ -0,0 +1,21 @@
+﻿using Gtk;
+using Graphics;
+using Engine;
+
+
+namespace Main
+{
+    class DescGen
+    {
+        private static GUI gui;
+        public static Creator creator;
+
+        public static void Main()
+        {
+            Application.Init();
+            creator = new Creator();
+            gui = new GUI();
+            Application.Run();
+        }
+    }
+}
